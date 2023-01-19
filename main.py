02b2796e7a9c3ae566a65b24616669e5c26041a2
vlad.shuisky <vlad.shuisky@gmail.com>
2023-01-19 13:35:53 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,12 @@
+from module import hello
+
+class Program:
+    def output_text(self):
+        print('text output')
+
+if __name__ == "__main__":
+    program = Program()
+    program.output_text()
+    hello()
+
+    
